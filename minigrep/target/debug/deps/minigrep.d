86f0f6db603e0e1a24a9855fcedac21deefcccfa
gratiaa <younggratia@gmail.com>
2020-12-27 16:07:47 +0900

    :sparkles: Create .gitignore, use template from github repo

--- minigrep/target/debug/deps/minigrep.d
+++
@@ -1,5 +0,0 @@
-/Users/young/Desktop/rust-lang-grep/minigrep/target/debug/deps/minigrep: src/main.rs
-
-/Users/young/Desktop/rust-lang-grep/minigrep/target/debug/deps/minigrep.d: src/main.rs
-
-src/main.rs:
--- minigrep/target/debug/minigrep.d
+++
@@ -1,1 +0,0 @@
-/Users/young/Desktop/rust-lang-grep/minigrep/target/debug/minigrep: /Users/young/Desktop/rust-lang-grep/minigrep/src/main.rs
