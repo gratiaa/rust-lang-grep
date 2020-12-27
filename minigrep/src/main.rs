ec836172f0b6a418e6ce712dcf554e606448a1ee
gratiaa <younggratia@gmail.com>
2020-12-27 16:33:25 +0900

    [12-4] Reading the contents of the file specified by the second arg

--- minigrep/src/main.rs
+++ minigrep/src/main.rs
@@ -1,13 +1,17 @@
 use std::env;
+// 파일을 다루는 표준 라이브러리를 추가한다.
+use std::fs;
 
-// 다음 명령어로 실행해보기: cargo run test sample.txt
+// 다음 명령어로 실행해보기: cargo run the poem.txt
 fn main() {
   let args: Vec<String> = env::args().collect();
 
-  // 다른 곳에서도 사용 가능할 수 있게 두 인자의 값을 저장한다.
-  let query = &args[1]; // &args[0]은 건너뛴다. -> 왜?
+  let query = &args[1]; // query는 다음에 사용한다.
   let filename = &args[2];
 
-  println!("Searching for {}", query);
   println!("In file {}", filename);
+
+  let contents = fs::read_to_string(filename).expect("Something went wrong while reading the file");
+
+  println!("With text:\n{}", contents)
 }
