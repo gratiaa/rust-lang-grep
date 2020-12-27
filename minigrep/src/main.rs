ac07b1d08a868a8f6b248cd37e71ad3c38a9f847
gratiaa <younggratia@gmail.com>
2020-12-27 17:03:02 +0900

    [12-6] Refactoring parse_config to return an instance of a Config struct

--- minigrep/src/main.rs
+++ minigrep/src/main.rs
@@ -1,47 +1,42 @@
 use std::env;
 use std::fs;
 
-/*
-  https://doc.rust-lang.org/book/ch12-03-improving-error-handling-and-modularity.html#separation-of-concerns-for-binary-projects
-
-  [바이너리 프로젝트 관심사 분리하기]
-
-  * main.rs와 lib.rs로 파일을 나눈 후에 lib.rs에 프로그램 로직을 옮긴다.
-    * command line parsing 로직 크기가 작다면 따로 파일을 나누지 않고, main.rs에 두어도 된다.
-    * 로직 크기가 커진다면 추출하여 lib.rs로 옮긴다.
-
-  위 과정을 거치면 main 함수에 남는 것은 다음과 같게 된다.
-
-  * 인자 값과 함께 command line parsing 로직 호출
-  * 기타 다른 설정값 세팅
-  * lib.rs의 run 실행
-  * run 함수가 에러를 반환하면 에러 처리
-*/
-
 // 다음 명령어로 실행해보기: cargo run the poem.txt
 fn main() {
   let args: Vec<String> = env::args().collect();
 
-  let (query, filename) = parse_config(&args);
+  let config = parse_config(&args);
 
-  println!("In file {}", filename);
+  println!("Searching for {}", config.query);
+  println!("In file {}", config.filename);
 
-  let contents = fs::read_to_string(filename).expect("Something went wrong while reading the file");
+  let contents =
+    fs::read_to_string(config.filename).expect("Something went wrong while reading the file");
 
   println!("With text:\n{}", contents);
 }
 
-fn parse_config(args: &[String]) -> (&str, &str) {
-  let query = &args[1];
-  let filename = &args[2];
-
-  (query, filename)
+struct Config {
+  query: String,
+  filename: String,
 }
 
-/*
-  문제: 위 코드에서 리팩토링 스멜이 나는 부분은?
+fn parse_config(args: &[String]) -> Config {
+  /*
+    args 변수의 주인(owner)은 main이며, 이를 parse_config에 빌려주고 있다.
+    Config가 args의 값에 대한 오너십을 가져가려고 한다면, Rust의 규칙을 위반한 셈이 된다.
 
-  답: https://doc.rust-lang.org/book/ch12-03-improving-error-handling-and-modularity.html#grouping-configuration-values
+    이를 예방하는 가장 쉬운 방법은 변수에서 clone 메소드를 호출하는 것이다.
+    문자열 데이터 참조값을 저장하는 시간과 메모리 비용보다 지출이 크지만
+    참조의 생명주기를 관리할 필요가 없기 때문에 코드가 매우 직관적이다.
+    -> 약간의 성능을 대가로 단순함을 얻는다.
 
-  * 토막 상식: primitive obsession
-*/
+    그렇지만 clone을 사용하는 것은 많은 러스트 사용자들이 지양하고 있는 방법인데, 런타임 비용이기 때문이다.
+    13장에서 이보다 더 효율적인 방법에 대해 알아본다.
+    여기서는 파일명과 쿼리 문자열이 아주 작고, 복사본을 한번만 사용하기 때문에 그냥 한다.
+  */
+  let query = args[1].clone();
+  let filename = args[2].clone();
+
+  Config { query, filename }
+}
