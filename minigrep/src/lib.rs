39d992d06e17d04b0a00cc8ea644a7ac12c166d9
gratiaa <younggratia@gmail.com>
2020-12-27 19:06:15 +0900

    [12-20] Adding a new failing test for the case-insensitive func

--- minigrep/src/lib.rs
+++ minigrep/src/lib.rs
@@ -47,14 +47,32 @@ mod tests {
   use super::*;
 
   #[test]
-  fn one_result() {
+  fn case_sensitive() {
     let query = "duct";
+    // 'Duct tape'는 'duct'를 포함하지 않은 것으로 판단해야 한다.
     let contents = "\
 Rust:
 safe, fast, productive.
-Pick three.";
+Pick three.
+Duct tape.";
 
     assert_eq!(vec!["safe, fast, productive."], search(query, contents));
+  }
+
+  #[test]
+  fn case_insensitive() {
+    let query = "rUsT";
+    // 'Trust me'는 'rUsT'를 포함한 것으로 판단해야 한다.
+    let contents = "\
+Rust:
+safe, fast, productive.
+Pick three.
+Trust me.";
+
+    assert_eq!(
+      vec!["Rust:", "Trust me."],
+      search_case_insensitive(query, contents)
+    );
   }
 }
 
