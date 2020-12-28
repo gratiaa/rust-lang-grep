3b6bfaaaa2ae88249b33e9f8d7d712b07cd9fe7b
gratiaa <younggratia@gmail.com>
2020-12-28 11:10:04 +0900

    [12-24] Writing Error messages to standard error

--- minigrep/src/main.rs
+++ minigrep/src/main.rs
@@ -9,21 +9,20 @@ use std::process;
 mod lib;
 use lib::Config;
 
-// 다음 명령어로 실행해보기: export CASE_INSENSITIVE=1; cargo run to poem.txt
-// 설정된 환경 변수 확인: echo $CASE_INSENSITIVE
-// 환경 변수 shell에서 제거: unset CASE_INSENSITIVE
+/*
+  다음 명령어로 실행해보기
+  1. cargo run > output.txt
+  2. cargo run to poem.txt > output.txt
+*/
 fn main() {
   let args: Vec<String> = env::args().collect();
   let config = Config::new(&args).unwrap_or_else(|err| {
-    println!("Problem parsing arguments: {}", err);
+    eprintln!("Problem parsing arguments: {}", err);
     process::exit(1);
   });
 
-  println!("Searching for {}", config.query);
-  println!("In file {}\n===================", config.filename);
-
   if let Err(e) = lib::run(config) {
-    println!("Application error: {}", e);
+    eprintln!("Application error: {}", e);
 
     process::exit(1);
   }
